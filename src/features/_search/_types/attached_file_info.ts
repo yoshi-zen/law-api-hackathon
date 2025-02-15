af794939357d3654e02acda8d62bbf1587229e5f
yoshi-zen <yoshi180705@ruri.waseda.jp>
2025-02-15 16:37:01 +0900

    attached_file_infoスキーマ作成

---
+++ src/features/_search/_types/attached_file_info.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { AttachedFileSchema } from "./attached_file";
+
+/**
+ * 添付ファイル情報を指す。
+ * image_data: Base64エンコードされた画像データ
+ * attached_files: 添付ファイル一覧
+ */
+export const AttachedFileInfoSchema = z.object({
+  image_data: z.string(),
+  attached_files: z.array(AttachedFileSchema),
+});
+
+export type AttachedFileInfoType = z.infer<typeof AttachedFileInfoSchema>;
