90af03326fd9b8c1f93b501f5f69e3349ac66751
yoshi-zen <yoshi180705@ruri.waseda.jp>
2025-02-16 14:00:34 +0900

    型情報の配置場所変更

--- src/features/_search/_components/input-condition-list/index.tsx
+++ src/features/_search/_components/input-condition-list/index.tsx
@@ -1,7 +1,12 @@
+import { Input } from "components/ui/input";
+
 export const InputConditionList = () => {
   return (
-    <div>
-      <p className="text-red-500">hoge</p>
+    <div className="flex flex-col gap-4 bg-gray-50">
+      <div className="rounded-md px-4 py-2">
+        <p>法令ID</p>
+        <Input />
+      </div>
     </div>
   );
 };
