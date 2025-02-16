00ff76b018e603b1b41bc692c25ec23531012bd4
Manchot13 <s.ono@toki.waseda.jp>
2025-02-16 23:57:29 +0900

    閉じタグをまとめた

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,28 +37,32 @@ export default function RootLayout({
         )}
       >
         <div className="grid grid-cols-[5fr_4fr] h-screen gap-4">
-          <div className="grid grid-cols-[1fr_4fr] grid-rows-[2.5rem_1fr] p-4 pr-0 h-screen gap-4">
-            <div className="bg-slate-400 rounded-md" />
-            <div className=" flex items-center rounded-md bg-slate-200 px-4">
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem>
-                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
-                  </BreadcrumbItem>
-                  <BreadcrumbSeparator>
-                    <Slash />
-                  </BreadcrumbSeparator>
-                  <BreadcrumbItem>
-                    <BreadcrumbLink href="/components">
-                      Components
-                    </BreadcrumbLink>
-                  </BreadcrumbItem>
-                </BreadcrumbList>
-              </Breadcrumb>
+          <div className="grid grid-cols-[1fr_4fr] p-4 pr-0 h-screen gap-4">
+            <div className="grid grid-rows-[2.5rem_1fr] gap-4">
+              <div className="bg-slate-400 rounded-md" />
+              <div className="bg-slate-600 rounded-md" />
             </div>
-            <div className="rounded-md" />
-            <div className="bg-slate-400 h-full flex-col overflow-y-scroll rounded-md">
-              {children}
+            <div className="grid grid-rows-[2.5rem_1fr] gap-4 ">
+              <div className=" flex items-center rounded-md bg-slate-200 px-4">
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem>
+                      <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator>
+                      <Slash />
+                    </BreadcrumbSeparator>
+                    <BreadcrumbItem>
+                      <BreadcrumbLink href="/components">
+                        Components
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+              <div className="bg-slate-400 max-h-[calc(100vh-5.5rem)] flex-col overflow-y-scroll rounded-md">
+                {children}
+              </div>
             </div>
           </div>
           <div className="rounded-md p-4 pl-0">
